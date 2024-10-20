e47aef86ccb32f902ba3e5d3580cdeeaa19ec379
Johan Frisk <jvf2609@gmail.com>
2024-10-21 01:19:45 +0300

    Hello - more features

--- hello/hello.go
+++ hello/hello.go
@@ -1,18 +1,30 @@
 package main
 
-import (
-	"fmt"
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
 )
 
-const greetingPrefix = "Hello, "
-
-func Hello(who string) string {
-	if who == "" {
-		who = "World"
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
 	}
-	return greetingPrefix + who
+
+	return greetingPrefix(language) + name
 }
 
-func main() {
-	fmt.Println(Hello(""))
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
